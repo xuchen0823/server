16dbd8be0a4576da3139e0cfeedf7d6099ffc66b
Zach Toogood <zrtoogood@gmail.com>
2022-06-15 18:51:24 +0300

    [core] Finish hooking up new settings in core

--- src/map/utils/moduleutils.cpp
+++ src/map/utils/moduleutils.cpp
@@ -178,13 +178,13 @@ namespace moduleutils
                 if (table["overrides"].valid()) // Check the file is a valid module
                 {
                     auto moduleName = table.get_or("name", std::string());
-                    ShowScript(fmt::format("=== Module: {} ===", moduleName));
+                    ShowInfo(fmt::format("=== Module: {} ===", moduleName));
                     for (auto& override : table.get_or("overrides", std::vector<sol::table>()))
                     {
                         std::string name = override["name"];
                         sol::object func = override["func"];
 
-                        ShowScript(fmt::format("Preparing override: {}", name));
+                        ShowInfo(fmt::format("Preparing override: {}", name));
 
                         auto parts = split(name, ".");
                         overrides.emplace_back(Override{ filename, name, parts, func, false });
@@ -215,7 +215,7 @@ namespace moduleutils
 
                     if (part == lastTable)
                     {
-                        ShowScript(fmt::format("Applying override: {}", override.overrideName));
+                        ShowInfo(fmt::format("Applying override: {}", override.overrideName));
 
                         lua["applyOverride"](table, lastElem, override.func, override.overrideName, override.filename);
 
